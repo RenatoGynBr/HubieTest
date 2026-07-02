19985498ea90cfc234ec17f4153249eb789bede8
Renato Cavalcante Silva <renatofh@hotmail.com>
2026-07-02 15:09:16 -0300

    TODO implemented in HubieTest ticket.ashx.cs.

--- HubieTest.Web/ashx/process/ticket.ashx.cs
+++ HubieTest.Web/ashx/process/ticket.ashx.cs
@@ -1,5 +1,6 @@
 using System.Web;
 using HubieTest.Business;
+using HubieTest.Dal;
 using HubieTest.Web.ashx;
 using Newtonsoft.Json;
 
@@ -53,41 +54,172 @@ namespace HubieTest.Web.process
 
             switch (method)
             {
+                // deserialize TICKET from "data", call business.open(...) and serialize
                 case "open":
-                    // TODO: deserialize TICKET from "data", call business.open(...) and serialize
-                    return notImplemented(method);
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "data is required" });
+                    }
 
+                    var ticketToOpen = JsonConvert.DeserializeObject<TICKET>(data);
+                    var created = business.open(ticketToOpen);
+                    if (business.hasError)
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = business.ErrorMessage });
+                    }
+
+                    return JsonConvert.SerializeObject(created);
+
+                // return JsonConvert.SerializeObject(business.listMyTickets());
                 case "listMine":
-                    // TODO: return JsonConvert.SerializeObject(business.listMyTickets());
-                    return notImplemented(method);
+                    return JsonConvert.SerializeObject(business.listMyTickets());
 
+                // read status (optional) from "data" and call business.listQueue(status)
                 case "listQueue":
-                    // TODO: read status (optional) from "data" and call business.listQueue(status)
-                    return notImplemented(method);
+                    string status = null;
+                    if (!string.IsNullOrEmpty(data))
+                    {
+                        var obj = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(data);
+                        status = (string)obj["status"];
+                    }
 
+                    return JsonConvert.SerializeObject(business.listQueue(status));
+
+                // read ticketId from "data" and call business.get(ticketId)
                 case "get":
-                    // TODO: read ticketId from "data" and call business.get(ticketId)
-                    return notImplemented(method);
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "data is required" });
+                    }
 
+                    var oget = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(data);
+                    if (!long.TryParse((string)oget["ticketId"], out long getId))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "ticketId is required and must be a number" });
+                    }
+
+                    var ticket = business.get(getId);
+                    if (business.hasError)
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = business.ErrorMessage });
+                    }
+
+                    return JsonConvert.SerializeObject(ticket);
+
+                // read ticketId from "data" and call business.assign(ticketId)
                 case "assign":
-                    // TODO: read ticketId from "data" and call business.assign(ticketId)
-                    return notImplemented(method);
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "data is required" });
+                    }
 
+                    var oassign = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(data);
+                    if (!long.TryParse((string)oassign["ticketId"], out long assignId))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "ticketId is required and must be a number" });
+                    }
+
+                    business.assign(assignId);
+                    if (business.hasError)
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = business.ErrorMessage });
+                    }
+
+                    var assigned = business.get(assignId);
+                    return JsonConvert.SerializeObject(assigned);
+
+                // read ticketId + status from "data" and call business.changeStatus(...)
                 case "changeStatus":
-                    // TODO: read ticketId + status from "data" and call business.changeStatus(...)
-                    return notImplemented(method);
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "data is required" });
+                    }
 
+                    var ochg = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(data);
+                    if (!long.TryParse((string)ochg["ticketId"], out long chgId) || string.IsNullOrEmpty((string)ochg["status"]))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "ticketId and status are required" });
+                    }
+
+                    string newStatus = (string)ochg["status"];
+                    business.changeStatus(chgId, newStatus);
+                    if (business.hasError)
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = business.ErrorMessage });
+                    }
+
+                    var changed = business.get(chgId);
+                    return JsonConvert.SerializeObject(changed);
+
+                // read ticketId + message from "data" and call business.addInteraction(...)
                 case "addInteraction":
-                    // TODO: read ticketId + message from "data" and call business.addInteraction(...)
-                    return notImplemented(method);
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "data is required" });
+                    }
 
+                    var oi = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(data);
+                    if (!long.TryParse((string)oi["ticketId"], out long itTicketId) || string.IsNullOrEmpty((string)oi["message"]))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "ticketId and message are required" });
+                    }
+
+                    string message = (string)oi["message"];
+                    var interaction = business.addInteraction(itTicketId, message);
+                    if (business.hasError)
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = business.ErrorMessage });
+                    }
+
+                    return JsonConvert.SerializeObject(interaction);
+
+                // read ticketId from "data" and call business.listInteractions(ticketId)
                 case "listInteractions":
-                    // TODO: read ticketId from "data" and call business.listInteractions(ticketId)
-                    return notImplemented(method);
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "data is required" });
+                    }
 
+                    var oli = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(data);
+                    if (!long.TryParse((string)oli["ticketId"], out long listIntId))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "ticketId is required and must be a number" });
+                    }
+
+                    return JsonConvert.SerializeObject(business.listInteractions(listIntId));
+
+                // read ticketId from "data" and call business.listAttachments(ticketId)
                 case "listAttachments":
-                    // TODO: read ticketId from "data" and call business.listAttachments(ticketId)
-                    return notImplemented(method);
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "data is required" });
+                    }
+
+                    var ola = JsonConvert.DeserializeObject<Newtonsoft.Json.Linq.JObject>(data);
+                    if (!long.TryParse((string)ola["ticketId"], out long listAttId))
+                    {
+                        HttpStatusReturn = 400;
+                        return JsonConvert.SerializeObject(new { error = "ticketId is required and must be a number" });
+                    }
+
+                    return JsonConvert.SerializeObject(business.listAttachments(listAttId));
 
                 default:
                     HttpStatusReturn = 400;
@@ -95,10 +227,10 @@ namespace HubieTest.Web.process
             }
         }
 
-        private string notImplemented(string method)
-        {
-            HttpStatusReturn = 501; // Not Implemented
-            return JsonConvert.SerializeObject(new { error = "Method not implemented yet: " + method });
-        }
+        //private string notImplemented(string method)
+        //{
+        //    HttpStatusReturn = 501; // Not Implemented
+        //    return JsonConvert.SerializeObject(new { error = "Method not implemented yet: " + method });
+        //}
     }
 }
