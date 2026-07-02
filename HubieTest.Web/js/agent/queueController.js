cc895678fbcbfc8959b8fc1df984ba15f880d0b1
Renato Cavalcante Silva <renatofh@hotmail.com>
2026-07-02 16:14:33 -0300

    TODO queueController.js implemented

--- HubieTest.Web/js/agent/queueController.js
+++ HubieTest.Web/js/agent/queueController.js
@@ -9,9 +9,32 @@ angular.module('hubieTest').controller('queueController',
         $scope.tickets = [];
         $scope.statusFilter = '';
 
+        $scope.loading = false;
+        $scope.error = null;
+
+        //$scope.load = function () {
+        // TODO: apiService.request('ashx/process/ticket.ashx', 'listQueue',
+        //       JSON.stringify({ status: $scope.statusFilter })).then(...)
+        //};
+
         $scope.load = function () {
-            // TODO: apiService.request('ashx/process/ticket.ashx', 'listQueue',
-            //       JSON.stringify({ status: $scope.statusFilter })).then(...)
+            $scope.loading = true;
+            $scope.error = null;
+
+            var statusFilter = $scope.statusFilter ? JSON.stringify({ status: $scope.statusFilter }) : null;
+
+            apiService.request('ashx/process/ticket.ashx', 'listQueue', statusFilter)
+                .then(function (r) {
+                    $scope.tickets = r.data || [];
+                })
+                .catch(function (err) {
+                    console.error('listQueue error', err);
+                    $scope.error = 'Unable to load queue';
+                    $scope.tickets = [];
+                })
+                .finally(function () {
+                    $scope.loading = false;
+                });
         };
 
         $scope.handle = function (ticket) {
