c9964a9ba7c517e29e6bc49d3c79b1f831467335
Renato Cavalcante Silva <renatofh@hotmail.com>
2026-07-02 17:45:50 -0300

    TODO ticketDetailController.js implementations

--- HubieTest.Web/js/requester/ticketDetailController.js
+++ HubieTest.Web/js/requester/ticketDetailController.js
@@ -13,15 +13,64 @@ angular.module('hubieTest').controller('ticketDetailController',
         $scope.attachments = [];
         $scope.newMessage = '';
 
+        $scope.loading = false;
+        $scope.error = null;
+        $scope.replyLoading = false;
+        $scope.replyError = null;
+
         $scope.load = function () {
-            // TODO: get ('get'), listInteractions, listAttachments
+            $scope.loading = true;
+            $scope.error = null;
+
+            apiService.request('ashx/process/ticket.ashx', 'get', JSON.stringify({ ticketId: $scope.ticketId }))
+                .then(function (r) {
+                    $scope.ticket = r.data;
+                    return apiService.request('ashx/process/ticket.ashx', 'listInteractions', JSON.stringify({ ticketId: $scope.ticketId }));
+                })
+                .then(function (r) {
+                    $scope.interactions = r.data || [];
+                    return apiService.request('ashx/process/ticket.ashx', 'listAttachments', JSON.stringify({ ticketId: $scope.ticketId }));
+                })
+                .then(function (r) {
+                    $scope.attachments = r.data || [];
+                })
+                .catch(function (err) {
+                    console.error('Load error', err);
+                    $scope.error = (err.data && err.data.error) ? err.data.error : 'Unable to load ticket data.';
+                })
+                .finally(function () {
+                    $scope.loading = false;
+                });
         };
 
         $scope.reply = function () {
-            // TODO: 'addInteraction' with $scope.newMessage and reload the thread
-            alert('TODO: implement reply (addInteraction).');
-        };
+            var msg = ($scope.newMessage || '').trim();
+            if (!msg) return;
 
+            $scope.replyLoading = true;
+            $scope.replyError = null;
+
+            apiService.request('ashx/process/ticket.ashx', 'addInteraction', JSON.stringify({ ticketId: $scope.ticketId, message: msg }))
+                .then(function () {
+                    $scope.newMessage = '';
+                    // reload interactions and ticket header
+                    return apiService.request('ashx/process/ticket.ashx', 'listInteractions', JSON.stringify({ ticketId: $scope.ticketId }));
+                })
+                .then(function (r) {
+                    $scope.interactions = r.data || [];
+                    return apiService.request('ashx/process/ticket.ashx', 'get', JSON.stringify({ ticketId: $scope.ticketId }));
+                })
+                .then(function (r) {
+                    $scope.ticket = r.data;
+                })
+                .catch(function (err) {
+                    console.error('Reply error', err);
+                    $scope.replyError = (err.data && err.data.error) ? err.data.error : 'Unable to send reply.';
+                })
+                .finally(function () {
+                    $scope.replyLoading = false;
+                });
+        };
         $scope.load();
     }
 ]);
